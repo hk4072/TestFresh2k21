3933b10c7d311f6f6f48988efcd0d0a8a16b35b8
Hassan Khan <hassan.khan@turnlunchon.com>
2021-08-30 16:58:45 +0500

    My Main Class has been changed

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,8 @@
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("asdhasdhasji");
+        System.out.println("asdhasdhasjiakdjhasljkdsajkldsajk");
+        System.out.println("asdhasdhasjiakdjhasljkdsajkldsajk");
+
     }
 }
