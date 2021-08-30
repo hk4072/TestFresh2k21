8e5b2107501cfdf03a99f04fcd14a2e6b5ee0200
Hassan Khan <hassan.khan@turnlunchon.com>
2021-08-30 17:02:08 +0500

    new Feature Added

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,6 +3,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("asdhasdhasjiakdjhasljkdsajkldsajk");
         System.out.println("asdhasdhasjiakdjhasljkdsajkldsajk");
-
+        TestClass.test();
     }
 }
